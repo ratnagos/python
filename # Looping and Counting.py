74b98167b311d73554f9e249a1e94cfd893e808e
Ratna Gosavi <ratnagosavi3@gmail.com>
2026-07-09 09:32:09 +0530

    #looping and counting .py

---
+++ # Looping and Counting.py
@@ -0,0 +1,8 @@
+# Looping and Counting 
+
+word = 'banana'      #statment1
+count = 0     #statment2
+for letter in word:    #statment3
+    if letter == 'a':   # sub statment1 
+        count = count + 1   #substatment2
+print(count)       #statment4
